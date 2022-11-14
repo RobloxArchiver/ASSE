39f8b6ee2033cc488435ef2c5aea107353034db4
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2022-11-14 13:20:03 -0500

    0.0.1b
    Variables for Formatting

--- src/main.lua
+++ src/main.lua
@@ -6,6 +6,13 @@ local clonefunc = clonefunction
 local print_c = clonefunc(print)
 local error_c = clonefunc(error)
 
+--> Variables
+local Workspace = game.Workspace
+local ReplicatedStorage = game.ReplicatedStorage
+local Players = game.Players
+local FindFirstChild = game.FindFirstChild
+local GetChildren = game.GetChildren
+
 --> Replace print with our own :cool:
 print = function(...)
     if rrkit_sdk and debugger == true then
