b1bcdea5d366c16b2dc6e3ddc626e71ffedfd87c
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2022-12-19 12:53:36 -0500

    Update CLi.lua

--- src/CLi.lua
+++ src/CLi.lua
@@ -1,7 +1,11 @@
-loadstring(syn.request({
-    Url = "https://raw.githubusercontent.com/RobloxArchiver/rrkit_release/main/src/main.lua",
-    Method = "GET"
-}).Body)()
+loadstring(
+    syn.request(
+        {
+            Url = "https://raw.githubusercontent.com/RobloxArchiver/rrkit_release/main/src/main.lua",
+            Method = "GET"
+        }
+    ).Body
+)()
 
 repeat
     wait()
@@ -13,22 +17,26 @@ function printc(...)
     console.print("[ASSE]: " .. ...)
 end
 
-handler:add("test", function()
-    local oldPos = game.Players.LocalPlayer.HumanoidRootPart.CFrame
-    
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(9e9, 9e9, 9e9)
-    console.error("Oops!")
-    console.success("Sorry!")
-    
-    wait(5)
-    
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = oldPos
-    printc("There we go!")
-    asse.print("Sorry about the CLi, currently nothing to add until other stuff are done. :(")
-end)
+handler:add(
+    "test",
+    function()
+        local oldPos = game.Players.LocalPlayer.HumanoidRootPart.CFrame
+
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(9e9, 9e9, 9e9)
+        console.error("Oops!")
+        console.success("Sorry!")
+
+        wait(5)
+
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = oldPos
+        printc("There we go!")
+        asse.print("Sorry about the CLi, currently nothing to add until other stuff are done. :(")
+    end
+)
 
 console.clear()
-console.print([[
+console.print(
+    [[
   /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$$$
  /$$__  $$ /$$__  $$ /$$__  $$| $$_____/
 | $$  \ $$| $$  \__/| $$  \__/| $$      
@@ -37,7 +45,9 @@ console.print([[
 | $$  | $$ /$$  \ $$ /$$  \ $$| $$      
 | $$  | $$|  $$$$$$/|  $$$$$$/| $$$$$$$$
 |__/  |__/ \______/  \______/ |________/
-]] .. "\n\n")
+]] ..
+        "\n\n"
+)
 
 printc("Loaded | Type 'help' or '?' for basic information.")
 
